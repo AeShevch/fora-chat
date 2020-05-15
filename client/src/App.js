2ee615103f1f7139caf4aa0385fb2febf34de998
AeShevch <Neron610>
2020-05-16 02:30:51 +0300

    refactor(App): Changes function into class

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,13 @@ import Chat from "./components/Chat/Chat";
 
 import "./App.scss";
 
-const App = () => (
-  <Router>
-    <Route path="/" exact component={Join} />
-    <Route path="/chat" exact component={Chat} />
-  </Router>
-);
-
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route path="/" exact component={Join} />
+        <Route path="/chat" exact component={Chat} />
+      </Router>
+    );
+  }
+}
